e90f26e658b2a3cc65e5cf0218265001009c9958
Luiz Henrique <luiiizce@gmail.com>
2024-03-05 20:23:02 -0300

    first commit

---
+++ src/api/get-month-canceled-orders-amount.ts
@@ -0,0 +1,15 @@
+import { api } from "@/lib/axios";
+
+export interface GetMonthCanceledOrdersAmountResponse {
+    diffFromLastMonth: number;
+    amount: number
+    diffFromYLastMonth: number
+}
+
+export async function getMonthCanceledOrdersAmount() {
+    const response = await api.get<GetMonthCanceledOrdersAmountResponse>(
+        '/metrics/month-canceled-orders-amount'
+    )
+
+    return response.data
+}
